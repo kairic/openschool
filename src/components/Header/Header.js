b186e31fdc35fd1894488d43f3d2bb879f1952c9
Kairic <kairic.cheng@gmail.com>
2016-08-28 23:00:55 +0800

    Updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,6 @@ import classes from './Header.scss'
 
 export const Header = () => (
   <div className='header'>
-    <h1>Testing</h1>
-  </div>
-  <div>
     <h1>React Redux Starter Kit</h1>
     <IndexLink to='/' activeClassName={classes.activeRoute}>
       Home
